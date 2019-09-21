104050eccde718218b23a59d5c31e2aedca50792
Francesc Pellicero <francesc@tiendeo.com>
2019-09-21 20:28:18 +0200

    Add block appear animation

--- src/Prefabs/Blocks/BaseRectangle.ts
+++ src/Prefabs/Blocks/BaseRectangle.ts
@@ -12,11 +12,25 @@ export default abstract class BaseRectangle extends Phaser.Physics.Arcade.Sprite
 
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
         super(scene, x, y, texture);
+        this.scale = 0;
 
         scene.add.existing(this);
         scene.physics.world.enableBody(this);
         
         this.setImmovable(true);
+
+        this.scene.add.tween({
+            targets: this,
+            delay: Math.random() * 1000,
+            props: {
+                scale: {
+                    getStart: () => 0,
+                    getEnd: () => 1,
+                    ease: "Bounce"
+                }
+            },
+            duration: Math.random() * 1000
+        });
     }    
 
     destroy() {
