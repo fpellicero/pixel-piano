74803f6c68096f4e08afabc97bd3388ed0f68b96
Francesc Pellicero <fpellicero@gmail.com>
2020-03-13 19:22:21 +0100

    Pages deploy script

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: "./src",
@@ -17,14 +15,6 @@ module.exports = {
                 exclude: /(node_modules|dist)/,
                 loader: 'ts-loader'
             },
-            {
-                test: /\.css$/i,
-                use: [{
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    'css-loader'
-                ],
-            },
         ]
     },
     optimization: {
@@ -37,8 +27,6 @@ module.exports = {
         new CopyPlugin([
             { from: "./assets", to: "./assets" }
         ]),
-        new MiniCssExtractPlugin(),
-        // new CleanWebpackPlugin(),
         /* new BundleAnalyzerPlugin({
             analyzerMode: "static"
         }) */
